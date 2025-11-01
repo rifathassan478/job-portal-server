bb5185d7584b708ebe50711e1a9d4a66c3a594ec
Rifat Hassan <rifathassanrana@gmail.com>
2025-11-02 00:22:18 +0600

    Created applications retrieval by email

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var cors = require("cors");
 
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -32,12 +32,41 @@ async function run() {
         await client.connect();
 
         const jobs = client.db("jobs").collection("jobs");
+        const applications = client.db("jobs").collection("applications");
 
+        // Get all jobs
         app.get("/jobs", async (req, res) => {
             const cursor = jobs.find();
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        // Get a single job by ID
+        app.get('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const job = await jobs.findOne(query);
+            res.send(job);
+        });
+
+        // Job Applications related API
+
+        // Insert an applicaton
+        app.post('/applications', async (req, res) => {
+            const application = req.body;
+            const result = await applications.insertOne(application);
+            res.send(result);
+        });
+
+        // Get applications by applicant email
+        app.get('/applications', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant: email };
+            const result = await applications.find(query).toArray();
+            res.send(result);
+        });
+ 
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
